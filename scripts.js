d401f41225bde860de15ae5a79e3b2f805b8b6e7
Phanuelle <phanmanuel9@gmail.com>
2025-06-16 11:13:55 -0500

    fixing app initialization

--- scripts.js
+++ scripts.js
@@ -6,10 +6,6 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-const db = getFirestore(app);
-window.db = db; // Make it global so createLikeButton can access it
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -36,6 +32,12 @@ const authors = {
     bio: "CS major, hackathon addict, big fan of VS Code themes.",
   },
 };
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+const db = getFirestore(app);
+window.db = db; // Make it global so createLikeButton can access it
+
+// END FIREBASE SETUP
 
 const posts = [
   {
