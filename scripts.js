d5f96b1c1d2718825f6574bfd4b4043f536b2fcc
Phanuelle <phanmanuel9@gmail.com>
2025-06-16 13:45:25 -0500

    fixing scripts module

--- scripts.js
+++ scripts.js
@@ -2,14 +2,14 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-// import {
-//   getFirestore,
-//   doc,
+import {
+  getFirestore,
+  doc,
 //   getDoc,
 //   setDoc,
 //   updateDoc,
 //   increment,
-// } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
