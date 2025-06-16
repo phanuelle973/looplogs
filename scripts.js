ebf7196f0d838d35a54e764e0fbb215fc7515d3b
Phanuelle <phanmanuel9@gmail.com>
2025-06-16 11:02:21 -0500

    adding firestore to js

--- scripts.js
+++ scripts.js
@@ -99,6 +99,11 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+const db = getFirestore(app);
+window.db = db; // Make it global so createLikeButton can access it
+
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
