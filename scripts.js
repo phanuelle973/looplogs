6b8c7ae00b8eda523672495f5a68ec07cab78800
Phanuelle <phanmanuel9@gmail.com>
2025-06-17 15:01:58 -0500

    displaying username

--- scripts.js
+++ scripts.js
@@ -268,8 +268,8 @@ async function renderAuthorPage() {
   const profileBox = document.getElementById('author-profile-page');
   if (profileBox) {
     profileBox.innerHTML = `
-      <img src="${author.image}" alt="${author.name || author.id}" style="max-width:120px;border-radius:50%;">
-      <h2>${author.name || author.id}</h2>
+      <img src="${author.image}" alt="${author.name || author.username}" style="max-width:120px;border-radius:50%;">
+      <h2>${author.username}</h2>
       <p>${author.bio || ''}</p>
       ${author.link ? `<a href="${author.link}" target="_blank">Profile</a>` : ''}
     `;
