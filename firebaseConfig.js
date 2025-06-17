f0cc81b37ac7f42325225bc7d1c8695ad27fcda8
Phanuelle <phanmanuel9@gmail.com>
2025-06-17 12:52:53 -0500

    updating firebase config even tho i thought i already did...

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,11 @@
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_MSG_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyDKaDLUXIFkorwaM9k_RvSiIip5z3rp8Zo",
+  authDomain: "looplogs-4a711.firebaseapp.com",
+  projectId: "looplogs-4a711",
+  storageBucket: "looplogs-4a711.firebasestorage.app",
+  messagingSenderId: "122145076851",
+  appId: "1:122145076851:web:afc68cf5c11c2c8ee95c06",
+  measurementId: "G-WXD0LKBL8N"
 };
 
 firebase.initializeApp(firebaseConfig);
